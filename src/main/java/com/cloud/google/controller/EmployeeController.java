412be0ae2819788d5f84103beffb9b9e747f4e8c
yogesh147 <yogeshch147@gmail.com>
2022-09-07 10:52:30 +0530

    change employee resource mapping name

--- src/main/java/com/cloud/google/controller/EmployeeController.java
+++ src/main/java/com/cloud/google/controller/EmployeeController.java
@@ -26,7 +26,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("monibag")
+@RequestMapping("company")
 public class EmployeeController {
 
     @Autowired
